92a73e5dabdbf36728d4c6c11b6cbb34e8bfe188
josephzegarelli <jtzegarelli@gmail.com>
2025-12-26 13:10:56 -0500

    solve pandas exercise

---
+++ funs.py
@@ -0,0 +1,10 @@
+def simple_calculator(a,b):
+    s = a+b
+    d = a-b
+    p = a*b
+    print(f"sum is {s}, difference is {d} and product is {p}")
+
+
+
+def greeting(name):
+    print(f"hello {name.title()} Welcome to python")
